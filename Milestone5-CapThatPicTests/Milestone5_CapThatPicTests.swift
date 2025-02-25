8f8b793925a0d8e11d14ab1a73d21abb6e86efdc
Noah Pope <npope@tutanota.com>
2025-02-25 13:59:14 -0600

    Initial Commit

---
+++ Milestone5-CapThatPicTests/Milestone5_CapThatPicTests.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone5_CapThatPicTests.swift
+//  Milestone5-CapThatPicTests
+//
+//  Created by Noah Pope on 2/25/25.
+//
+
+import Testing
+@testable import Milestone5_CapThatPic
+
+struct Milestone5_CapThatPicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Milestone5-CapThatPicUITests/Milestone5_CapThatPicUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Milestone5_CapThatPicUITestsLaunchTests.swift
+//  Milestone5-CapThatPicUITests
+//
+//  Created by Noah Pope on 2/25/25.
+//
+
+import XCTest
+
+final class Milestone5_CapThatPicUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
