b49ac1a733a78a94121bbc676d4e99dcd71c6054
Noah Pope <npope@tutanota.com>
2025-02-26 10:43:14 -0600

    embedding homevc into nav controller

--- Milestone5-CapThatPic/Screens/HomeTableVC.swift
+++ Milestone5-CapThatPic/Screens/HomeTableVC.swift
@@ -12,7 +12,7 @@ class HomeTableVC: UIViewController, UIImagePickerControllerDelegate
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("heyz")
     }
 
 
